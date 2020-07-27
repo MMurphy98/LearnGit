4c74789d24e29777cba8151ab2a2cb84365b615e
MMurphy98 <mjg654654@outlook.com>
2020-07-27 09:00:18 +0800

    set up debugger on vscode (compile by WSL2)

--- hello.c
+++ hello.c
@@ -2,5 +2,14 @@
 
 int main(){
     printf("Hello world!\n");
+    
+    size_t n = 10;
+    int a[n];
+    for(int i = 0; i < 10; i++){
+        a[i] = i+1;
+    }
+
+    printf("Done\r\n");
+
     return 0;
 }
